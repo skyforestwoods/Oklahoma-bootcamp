e98cf10ffdc73e2505d2035b5e51014a117f899a
Tengfei Lin <tengfei.lin@ou.edu>
2013-07-01 15:37:13 -0400

    Small tweaks to the code

--- add_numb.py
+++ add_numb.py
@@ -1,11 +1,12 @@
 #This has been authored by Tengfei Lin but who knows who will contribute to it in the future
-from adding import add_two, add_three
+#from adding import add_two, add_three
 
-#def add_two(a,b):
-#    return a+b
+def add_two(a,b):
+    print ("Hi, I'm just adding two numbers here")
+    return a+b
     
 def main():
-    print ("2+2+2=", add_three(2,2,2))
+    print ("2+2+2=", add_two(2,2,2))
     print ("2+3=", add_two(2,3))
     print ("hello Oklahoma")
 
