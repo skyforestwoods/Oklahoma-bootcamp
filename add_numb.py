4da7fe4c7be2391017cce275a42b69a83c75eb81
Tengfei Lin <tengfei.lin@ou.edu>
2013-07-01 15:29:50 -0400

    Added module with functions

--- add_numb.py
+++ add_numb.py
@@ -1,8 +1,12 @@
-def add_two(a,b):
-    return a+b
+#This has been authored by Tengfei Lin but who knows who will contribute to it in the future
+from adding import add_two, add_three
+
+#def add_two(a,b):
+#    return a+b
     
 def main():
-    print ("2+3=", add_two(2,3))
+    print ("2+2+2=", add_three(2,2,2))
+    print ("2+4=", add_two(2,4))
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
---
+++ adding.py
@@ -0,0 +1,6 @@
+def add_three(x, y, z):
+    print x+y+x
+
+def add_two(x, y):
+    print x+y
+
